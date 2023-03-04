60c40613326709550f15ec8735f5967c2cc6187f
Akaccia <113586740+Akaccia@users.noreply.github.com>
2023-03-05 01:44:55 +0800

    Update hy.sh

--- hy.sh
+++ hy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt update
+sudo apt-get update
 apt -y install curl 
 bash <(curl  -fsSL https://git.io/hysteria.sh)
 wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh
