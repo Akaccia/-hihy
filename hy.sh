6731f9fb2c31970e9cfa130a6440c7471b41dc14
Akaccia <113586740+Akaccia@users.noreply.github.com>
2023-04-19 23:54:56 +0800

    Update hy.sh

--- hy.sh
+++ hy.sh
@@ -4,3 +4,4 @@
  apt -y install curl
  wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh 4
  bash <(curl  -fsSL https://git.io/hysteria.sh)
+ echo "precedence ::ffff:0:0/96 100" >> /etc/gai.conf
